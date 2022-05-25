42e8b583e52ef5ed4980b98cf513875d5bc4a4f9
Vitalii_Kyryliuk <Vitalii_Kyryliuk@epam.com>
2022-05-25 12:59:59 +0300

    Task #2: Webpack # clear project

--- src/components/ClassComponentExamplePure.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class ClassComponentExamplePure extends React.PureComponent {
-  render() {
-    return <h2>Hey! This comes from Pure ClassComponent</h2>;
-  }
-}
-
-export default ClassComponentExamplePure;
