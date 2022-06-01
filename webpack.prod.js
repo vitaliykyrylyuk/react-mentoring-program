c6be6a075199ef29bb01deba492a89fe6557d584
Vitalii_Kyryliuk <Vitalii_Kyryliuk@epam.com>
2022-06-01 16:45:01 +0300

    Task #3: Components # install tailwind

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,23 +1,22 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 const TerserWebpackPlugin = require('terser-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
   plugins: [
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: '[name].css'
+      filename: '[name].[contenthash].css'
     })
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader'
-        ]
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
       }
     ]
   },
