e1ed5a16329d0b3d728ab0cc671c7803ee764a65
Vitalii_Kyryliuk <Vitalii_Kyryliuk@epam.com>
2022-06-16 05:28:22 +0300

    Task #4: Components # fixes after review: fixes of layout, svgs to components, split bigger components to smaller parts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat', ...defaultTheme.fontFamily.sans]
+      },
+      colors: {
+        rose: {
+          400: '#f65261'
+        },
+        gray: {
+          600: '#424242',
+          900: '#232323'
+        }
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
